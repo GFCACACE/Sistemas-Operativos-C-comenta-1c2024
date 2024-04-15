2499cc6284be0ed32b07b6e325d0ee023543571e
GFCACACE <gcacace@frba.utn.edu.ar>
2024-04-15 19:23:47 +0000

    Tag hecho

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -8,6 +8,8 @@ int main(int argc, char** argv) {
     //Iniciamos mediante una funcion las config, el logger y las conexiones
     //Asignamos un flag que nos devolverá si se pudo iniciar el módulo o no.
     bool flag_modulo = iniciar_kernel(argv[1]);
+    
+    
     //En caso de que no se haya inicializado, finalizamos el programa
     if(flag_modulo == false){
         finalizar_kernel();
