ad9984cd2b50b24cc7a4c29a0d92ce93d5050ddf
GFCACACE <gcacace@frba.utn.edu.ar>
2024-06-28 21:55:05 +0000

    ankara DialFS

--- entradasalida/src/dialfs.c
+++ entradasalida/src/dialfs.c
@@ -1,3 +1,18 @@
 #include "dialfs.h"
+FILE *archivo_bitmap,*archivo_metadata,*archivo_bloques;
+int tamanio_filesystem;
+t_config_io* config;
 
+bool iniciar_archivos_base(){
+    int i;
+    tamanio_filesystem = config->BLOCK_SIZE * config->BLOCK_COUNT;
+    archivo_bloques = fopen(PATH_BLOQUES,"w");
+    archivo_bitmap = fopen(PATH_BITMAP,"w");
+    for(i=0;i<tamanio_filesystem;i++) fprintf(archivo_bloques,'\0');
+    archivo_bitmap = fopen(PATH_BITMAP,"w");
+    for (i=0;i < config->BLOCK_COUNT;i++) fprintf(archivo_bitmap,"0");
+    fclose(archivo_bitmap);
+    fclose(archivo_bloques);
+    return true;
 
+}
--- entradasalida/src/dialfs.h
+++ entradasalida/src/dialfs.h
@@ -1,11 +1,18 @@
 #ifndef entradasalida_h
 #define entradasalida_h
-#include <entradasalida.h>
+#include "entradasalida.h"
+#include <stdlib.h>
+#define PATH_BITMAP "${workspaceFolder}/files_dialFS/bitmap.dat"
+#define PATH_BLOQUES "${workspaceFolder}/files_dialFS/bloques.dat"
+#define DIR_METADATA "${workspaceFolder}/files_dialFS/metadata/"
 
-bool crear_archivos_base();//Control de bloques de datos
+bool iniciar_archivos_base();//Control de bloques de datos
 bool compactacion();
 bool io_fs_create():
 bool io_fs_delete();
 bool io_fs_write();
 bool io_fs_read();
+
+extern FILE *archivo_bitmap,*archivo_metadata,*archivo_bloques;
+extern int tamanio_filesystem;
 #endif
