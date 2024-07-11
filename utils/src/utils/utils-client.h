b8a308fcde37422b5c61bdd92295c40342303f0f
brunojaime <bruno.jaime@live.com.ar>
2024-07-11 00:20:05 +0000

    Memoria refactorizad

--- utils/src/utils/utils-client.h
+++ utils/src/utils/utils-client.h
@@ -31,6 +31,6 @@ void enviar_direcciones_proceso(t_direcciones_proceso* direcciones_proceso,op_co
 
 void enviar_stream(void*stream,int size,int socket,op_code codigo_operacion);
 void _enviar_stream_(void* texto,int size,int socket,op_code operacion);
-t_buffer* leer_memoria(t_direcciones_proceso* direcciones_fisicas_registros,int conexion);
+
 t_buffer* crear_buffer(size_t size);
 #endif /* utils_client_h */
