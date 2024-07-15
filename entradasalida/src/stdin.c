01613e4c220792862737327e56ccd311a4f38a79
ATrossero <atrossero@frba.utn.edu.ar>
2024-07-15 18:43:43 +0000

    Agregado comentario previo a STDIN, indicando al usuario escribir

--- entradasalida/src/stdin.c
+++ entradasalida/src/stdin.c
@@ -10,6 +10,7 @@ void io_stdin_read(t_direcciones_proceso* direcciones_proceso, int conexion){
     char* texto = malloc(50);
     t_pid_valor pid_valor = direcciones_proceso->pid_size_total;
     char* texto_a_enviar = malloc(pid_valor.valor);
+    loguear("Escriba el texto deseado:");
     texto = leer_texto_consola();
     while(strlen(texto)  < pid_valor.valor){
         loguear_error("La cadena escrita es de %d bytes, debe ser de %d bytes.",strlen(texto),pid_valor.valor);
